4580cf970e54471e13e6792fc460edcd8a24ec75
ryanmcdonough <me@ryanmcdonough.co.uk>
2022-06-20 18:45:45 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>annaptaszynski.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+You can email podcast@qi.com
         </p>
       </main>
 
