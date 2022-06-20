7ae77252443ad084ce7c337417afa49e9f6fe7f2
ryanmcdonough <me@ryanmcdonough.co.uk>
2022-06-20 20:45:59 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} />. Not even remotely related to <a href="https://www.nosuchthingasafish.com">the podcast.</a>
       </footer>
     </>
   )
